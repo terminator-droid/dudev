e68d72cc5c715cab8e639bdcb0d9708a9635ad1f
ussdaniil <daniil.us@zyfra.com>
2023-02-06 10:40:24 +0300

    maven configuration added

---
+++ common/src/test/java/com/dudev/CommonIT.java
@@ -0,0 +1,13 @@
+package com.dudev;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CommonTI {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
