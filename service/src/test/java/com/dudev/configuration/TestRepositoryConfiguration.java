1e22e180dff55058e29998e2cc58c5bbf131288e
ussdaniil <daniil.us@zyfra.com>
2023-03-24 17:36:37 +0300

    springBoot configuration added

--- service/src/test/java/com/dudev/configuration/TestRepositoryConfiguration.java
+++ service/src/test/java/com/dudev/configuration/TestRepositoryConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+
 @Configuration
 @Import(RepositoryConfiguration.class)
 public class TestRepositoryConfiguration {
