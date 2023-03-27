51b6606fe158938379383ba8f8acc837fff97f59
ussdaniil <daniil.us@zyfra.com>
2023-03-27 16:21:59 +0300

    springBoot configured, commented code deleted

--- service/src/main/java/com/dudev/configuration/RepositoryConfiguration.java
+++ service/src/main/java/com/dudev/configuration/RepositoryConfiguration.java
@@ -16,25 +16,4 @@ import java.lang.reflect.Proxy;
 @Configuration
 @ComponentScan(basePackages = "com.dudev.dao")
 public class RepositoryConfiguration {
-
-//    @Bean
-//    org.hibernate.cfg.Configuration configuration() {
-//        return HibernateUtil.buildConfiguration();
-//    }
-//
-//    @Bean
-//    SessionFactory sessionFactory(org.hibernate.cfg.Configuration configuration) {
-//        return configuration.buildSessionFactory();
-//    }
-//
-//    @Bean
-//    EntityManager entityManager(SessionFactory sessionFactory) {
-//        return (EntityManager) Proxy.newProxyInstance(sessionFactory.getClass().getClassLoader(), new Class[]{Session.class},
-//                (proxy, method, args) -> method.invoke(sessionFactory.getCurrentSession(), args));
-//    }
-//
-//    @PreDestroy
-//    void destroy() {
-//        sessionFactory(configuration()).close();
-//    }
 }
--- service/src/test/java/com/dudev/configuration/TestRepositoryConfiguration.java
+++ service/src/test/java/com/dudev/configuration/TestRepositoryConfiguration.java
@@ -11,9 +11,4 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(RepositoryConfiguration.class)
 public class TestRepositoryConfiguration {
-
-//    @Bean
-//    SessionFactory sessionFactory() {
-//        return HibernateTestUtil.buildSessionFactory();
-//    }
 }
